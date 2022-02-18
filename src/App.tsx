21b561b69aa335708a0eb528417bde549bdfed88
Choi Phil <philchoi@icloud.com>
2022-02-18 10:57:30 +0900

    adding react-redux-firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react'
+import { StatusBar } from 'react-native'
+import { ThemeProvider } from 'react-native-magnus'
+
+import Theme from '@/Theme'
 
 import Navigator from './Navigators'
+import RRFProvider from './Store'
 
 export default function ({}) {
-  return <Navigator />
+  return (
+    <RRFProvider>
+      <ThemeProvider theme={Theme.default}>
+        <StatusBar barStyle="dark-content" />
+        <Navigator />
+      </ThemeProvider>
+    </RRFProvider>
+  )
 }
--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react'
-import { Div, Text } from 'react-native-magnus'
+import { Div, Text, Input, Button } from 'react-native-magnus'
 
 export default function Login({}) {
   return (
     <Div flex={1} alignItems="center" justifyContent="center">
       <Text>Login Screen</Text>
+      <Input placeholder="Username" />
+      <Input placeholder="Password" />
+      <Button
+        onPress={() => {
+          console.log('do something')
+        }}>
+        <Text>Login</Text>
+      </Button>
     </Div>
   )
 }
