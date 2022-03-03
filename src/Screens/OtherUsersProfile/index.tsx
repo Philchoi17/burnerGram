fb444f826a40c127366f3bd99ac7dfb0c69a5ceb
Choi Phil <philchoi@icloud.com>
2022-03-03 12:54:10 +0900

    almost finished with other user profile

--- src/Screens/OtherUsersProfile/index.tsx
+++ src/Screens/OtherUsersProfile/index.tsx
@@ -16,6 +16,8 @@ import { Image, Text, Button, Icon } from '@/Components'
 import { getFirestoreRef } from '@/Utils/Misc'
 import { COLLECTION_NAMES, DOC_KEYS } from '@/Constants/FIRE_NAMES'
 import Logger from '@/Utils/Logger'
+import { profileType } from '@/Types'
+import { PhotoTile } from '@/Components/Tiles'
 
 const { useState, useEffect } = React
 export default function OtherUsersProfile({}) {
@@ -26,7 +28,13 @@ export default function OtherUsersProfile({}) {
   const { params }: any = useRoute()
   const { userId } = params
 
-  // const { }
+  // state variables
+  const [activity, setActivity] = useState<boolean>(false)
+  const [otherUsersProfile, setOtherUsersProfile] = useState<
+    profileType | null | undefined | any
+  >(null)
+  const [usersPosts, setUsersPosts] = useState<any[] | null | undefined>(null)
+
   const { get } = firestore
 
   const getOtherUserProfile = async () => {
@@ -34,22 +42,50 @@ export default function OtherUsersProfile({}) {
       const ref = getFirestoreRef(`${COLLECTION_NAMES.USERS}/${userId}`)
       const doc = await ref.get()
       const data = doc.data()
-      Logger.debug('data =', data)
-    } catch (error) {}
+      setOtherUsersProfile(data)
+    } catch (error) {
+      Logger.error('error =', error)
+    }
   }
 
   const getUsersPosts = async () => {
     try {
-    } catch (error) {}
+      const getUsersPosts = await get({
+        collection: COLLECTION_NAMES.FEED_POSTS,
+        where: [[DOC_KEYS.USER_ID, '==', userId]],
+        orderBy: [DOC_KEYS.UPDATED_AT, 'desc'],
+      })
+      const gotUsersPosts = await getUsersPosts.docs.map((doc: any) =>
+        doc.data(),
+      )
+      setUsersPosts(gotUsersPosts)
+    } catch (error) {
+      Logger.error('error =', error)
+    }
   }
 
   const otherUsersProfileUseEffectHandler = () => {
     Logger.debug('otherUsersProfileUseEffectHandler')
+    getOtherUserProfile()
+    getUsersPosts()
+    return () => {
+      // cleanup
+    }
   }
 
   useEffect(otherUsersProfileUseEffectHandler, [])
 
   const { profile } = useAppSelector(({ firebase }) => firebase)
+
+  // photoURL,
+  // nickname,
+  // bio,
+  // navigateToEditProfile,
+  // earnedPress,
+  // creditsPress,
+  // postCount,
+  // credits,
+  // earnedSupport,
 
   return (
     <MainContainer
@@ -58,9 +94,36 @@ export default function OtherUsersProfile({}) {
       }}>
       <ScrollView showsVerticalScrollIndicator={false}>
         <Div p="md">
-          {/* <ProfileCard 
-            profile={profile}
-          /> */}
+          <ProfileCard
+            profile={otherUsersProfile}
+            photoURL={otherUsersProfile?.photoURL}
+            nickname={otherUsersProfile?.nickname}
+            bio={otherUsersProfile?.bio}
+            navigateToEditProfile={() => {}}
+            earnedPress={() => {}}
+            creditsPress={() => {}}
+            postCount={usersPosts?.length ?? 0}
+            credits={otherUsersProfile?.credits}
+            earnedSupport={otherUsersProfile?.earnedSupport}
+            otherUser
+          />
+          <Div
+            // borderWidth={1}
+            flexWrap="wrap"
+            row
+            flex={1}
+            p="xs"
+            alignItems="flex-start"
+            justifyContent="flex-start">
+            {usersPosts &&
+              usersPosts.map((post, idx) => (
+                <PhotoTile
+                  key={String(idx)}
+                  source={post.downloadURL}
+                  onPress={() => {}}
+                />
+              ))}
+          </Div>
         </Div>
       </ScrollView>
     </MainContainer>
