d07e462fe9efbc71a4ba7fec883279a2adb9a80b
Choi Phil <philchoi@icloud.com>
2022-03-22 14:28:06 +0900

    changing notification to class

--- src/Utils/Notifications/index.ts
+++ src/Utils/Notifications/index.ts
@@ -1,3 +1,78 @@
+// import messaging, {
+//   FirebaseMessagingTypes,
+// } from '@react-native-firebase/messaging'
+
+// import Logger from '../Logger'
+// import Config from '@/Config'
+
+// const { AuthorizationStatus } = messaging
+// async function requestUserPermission() {
+//   try {
+//     const authStatus = await messaging().requestPermission()
+//     const enabled =
+//       authStatus === AuthorizationStatus.AUTHORIZED ||
+//       authStatus === AuthorizationStatus.PROVISIONAL
+
+//     if (enabled) {
+//       Logger.debug('Authorization: status', authStatus)
+//     }
+//     Logger.debug('Authorization: enabled', authStatus)
+//     return authStatus
+//   } catch (error) {
+//     Logger.error(error)
+//   }
+// }
+
+// async function getFCMToken() {
+//   try {
+//     const hasToken = await Config.getFCMToken()
+//     if (hasToken) {
+//       Logger.debug('getFCMToken: token =', hasToken)
+//       return
+//     }
+//     const fcmToken = await messaging().getToken()
+//     Logger.debug('getFCMToken: fcmToken =', fcmToken)
+
+//     await Config.setFCMToken(fcmToken)
+//   } catch (error) {
+//     Logger.error('getFCMToken: error: =', error)
+//   }
+// }
+
+// function notificationListener() {
+//   messaging().onNotificationOpenedApp(
+//     (remoteMessage: FirebaseMessagingTypes.RemoteMessage) => {
+//       Logger.debug(
+//         'Notification caused app to open from background state:',
+//         remoteMessage.notification,
+//       )
+//     },
+//   )
+
+//   messaging()
+//     .getInitialNotification()
+//     .then((remoteMessage: FirebaseMessagingTypes.RemoteMessage | null) => {
+//       if (remoteMessage) {
+//         Logger.debug(
+//           'Notification caused app to open from quit state:',
+//           remoteMessage.notification,
+//         )
+//       }
+//     })
+
+//   messaging().onMessage(
+//     async (remoteMessage: FirebaseMessagingTypes.RemoteMessage) => {
+//       Logger.debug('Message received on foreground state: ...', remoteMessage)
+//     },
+//   )
+// }
+
+// export default {
+//   requestUserPermission: () => requestUserPermission(),
+//   getFCMToken: () => getFCMToken(),
+//   notificationListener: () => notificationListener(),
+// }
+
 import messaging, {
   FirebaseMessagingTypes,
 } from '@react-native-firebase/messaging'
@@ -6,69 +81,69 @@ import Logger from '../Logger'
 import Config from '@/Config'
 
 const { AuthorizationStatus } = messaging
-async function requestUserPermission() {
-  try {
-    const authStatus = await messaging().requestPermission()
-    const enabled =
-      authStatus === AuthorizationStatus.AUTHORIZED ||
-      authStatus === AuthorizationStatus.PROVISIONAL
+class Notifications {
+  constructor() {}
 
-    if (enabled) {
-      Logger.debug('Authorization: status', authStatus)
+  public async requestUserPermission() {
+    try {
+      const authStatus = await messaging().requestPermission()
+      const enabled =
+        authStatus === AuthorizationStatus.AUTHORIZED ||
+        authStatus === AuthorizationStatus.PROVISIONAL
+
+      if (enabled) {
+        Logger.debug('Authorization: status', authStatus)
+      }
+      Logger.debug('Authorization: enabled', authStatus)
+      return authStatus
+    } catch (error) {
+      Logger.error(error)
     }
-    Logger.debug('Authorization: enabled', authStatus)
-    return authStatus
-  } catch (error) {
-    Logger.error(error)
   }
-}
 
-async function getFCMToken() {
-  try {
-    const hasToken = await Config.getFCMToken()
-    if (hasToken) {
-      Logger.debug('getFCMToken: token =', hasToken)
-      return
+  public async getFCMToken() {
+    try {
+      const hasToken = await Config.getFCMToken()
+      if (hasToken) {
+        Logger.debug('getFCMToken: token =', hasToken)
+        return
+      }
+      const fcmToken = await messaging().getToken()
+      Logger.debug('getFCMToken: fcmToken =', fcmToken)
+
+      await Config.setFCMToken(fcmToken)
+    } catch (error) {
+      Logger.error('getFCMToken: error: =', error)
     }
-    const fcmToken = await messaging().getToken()
-    Logger.debug('getFCMToken: fcmToken =', fcmToken)
-
-    await Config.setFCMToken(fcmToken)
-  } catch (error) {
-    Logger.error('getFCMToken: error: =', error)
   }
-}
 
-function notificationListener() {
-  messaging().onNotificationOpenedApp(
-    (remoteMessage: FirebaseMessagingTypes.RemoteMessage) => {
-      Logger.debug(
-        'Notification caused app to open from background state:',
-        remoteMessage.notification,
-      )
-    },
-  )
-
-  messaging()
-    .getInitialNotification()
-    .then((remoteMessage: FirebaseMessagingTypes.RemoteMessage | null) => {
-      if (remoteMessage) {
+  public notificationListener() {
+    messaging().onNotificationOpenedApp(
+      (remoteMessage: FirebaseMessagingTypes.RemoteMessage) => {
         Logger.debug(
-          'Notification caused app to open from quit state:',
+          'Notification caused app to open from background state:',
           remoteMessage.notification,
         )
-      }
-    })
+      },
+    )
 
-  messaging().onMessage(
-    async (remoteMessage: FirebaseMessagingTypes.RemoteMessage) => {
-      Logger.debug('Message received on foreground state: ...', remoteMessage)
-    },
-  )
+    messaging()
+      .getInitialNotification()
+      .then((remoteMessage: FirebaseMessagingTypes.RemoteMessage | null) => {
+        if (remoteMessage) {
+          Logger.debug(
+            'Notification caused app to open from quit state:',
+            remoteMessage.notification,
+          )
+        }
+      })
+
+    messaging().onMessage(
+      async (remoteMessage: FirebaseMessagingTypes.RemoteMessage) => {
+        Logger.debug('Message received on foreground state: ...', remoteMessage)
+      },
+    )
+  }
 }
 
-export default {
-  requestUserPermission: () => requestUserPermission(),
-  getFCMToken: () => getFCMToken(),
-  notificationListener: () => notificationListener(),
-}
+export default new Notifications()
