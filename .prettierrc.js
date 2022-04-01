026a82631e63c9630f1cedeee63ac0c0f2ac2c04
Choi Phil <philchoi@icloud.com>
2022-04-01 18:34:11 +0900

    safetly

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,13 @@
 module.exports = {
+<<<<<<< ours
   bracketSpacing: true,
   jsxBracketSameLine: true,
   semi: false,
+=======
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+>>>>>>> theirs
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'all',
